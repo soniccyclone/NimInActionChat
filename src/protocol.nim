7722640ba8e4f4600d3f2fdf8e19a309b8210c3b
Nathan Barlow <nathanjbarlow@gmail.com>
2022-11-30 08:24:05 -0600

    Complete 3.4

--- src/protocol.nim
+++ src/protocol.nim
@@ -7,3 +7,31 @@ type
 
 proc parseMessage*(data: string): Message =
   let dataJson = parseJson(data)
+  result.username = dataJson["username"].getStr()
+  result.message = dataJson["message"].getStr()
+
+proc createMessage*(username, message: string): string =
+  result = $(%{
+    "username": %username,
+    "message": %message
+  }) & "\c\l"
+
+when isMainModule:
+  block:
+    let data = """{"username": "John", "message": "Hi!"}"""
+    let parsed = parseMessage(data)
+    doAssert parsed.username == "John"
+    doAssert parsed.message == "Hi!"
+  block:
+    let data = """foobar"""
+    try:
+      discard parseMessage(data)
+      doAssert false
+    except JsonParsingError:
+      doAssert true
+    except:
+      doAssert false
+  block:
+    let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
+    doAssert createMessage("dom", "hello") == expected
+  echo("All tests passed!")
